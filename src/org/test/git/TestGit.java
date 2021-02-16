a2e5e363a9749c56e01998a64129e917416b628a
VigneshPak <vickycena03@gmail.com>
2021-02-16 14:59:50 +0530

    Testing

---
+++ src/org/test/git/TestGit.java
@@ -0,0 +1,14 @@
+package org.test.git;
+
+public class TestGit {
+	
+	private static void method() {
+		System.out.println("Testing Git");
+	}
+
+	
+	public static void main(String[] args) {
+	
+		method();
+	}
+}
