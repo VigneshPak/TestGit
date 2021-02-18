30ed2806dd9caa3e5f10dbefe2a96713bd00f5d9
VigneshPak <vickycena03@gmail.com>
2021-02-18 10:58:06 +0530

    Haffy codes are done

--- src/org/test/git/TestGit.java
+++ src/org/test/git/TestGit.java
@@ -6,9 +6,13 @@ public class TestGit {
 		System.out.println("Testing Git");
 	}
 
+	private static void method2() {
+		System.out.println("Pushing code into Haffy branch");
+	}
 	
 	public static void main(String[] args) {
 	
 		method();
+		method2();
 	}
 }
