bf20b1ccc5e762372c26624b19eb696e79ec1a4e
VigneshPak <vickycena03@gmail.com>
2021-05-16 13:11:08 +0530

    DAY 1 Task Done

--- src/org/test/git/TestGit.java
+++ src/org/test/git/TestGit.java
@@ -1,24 +1,41 @@
 package org.test.git;
 
 public class TestGit {
-	
+
 	private static void method() {
 		System.out.println("Testing Git");
-	}
-	private static void method1() {
-		System.out.println("Pushing code into Vicky branch");
 	}
 
 	private static void method2() {
 		System.out.println("Pushing code into Haffy branch");
 	}
 
+	private static void method1() {
+		System.out.println("Pushing code into Vicky branch");
+	}
+
+	private static void day1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void day2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void day3() {
+		// TODO Auto-generated method stub
+
+	}
 
 	public static void main(String[] args) {
-	
-		method();
-		method1();
-		method2();
 
+		method();
+		method2();
+		method1();
+		day1();
+		day2();
+		day3();
 	}
 }
