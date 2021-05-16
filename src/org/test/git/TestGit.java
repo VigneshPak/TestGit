c9cfc2de7041711cc4d2299203dcc50c312a0ed2
VigneshPak <vickycena03@gmail.com>
2021-05-16 13:26:31 +0530

    Toady codes done for vicky

--- src/org/test/git/TestGit.java
+++ src/org/test/git/TestGit.java
@@ -9,9 +9,21 @@ public class TestGit {
 	private static void method1() {
 		System.out.println("Pushing code into Vicky branch");
 	}
+	
+	private static void day1Vicky1() {
+		System.out.println("Day 1 vicky 2 codes Pushing into Vicky branch");
+	}
+	
+	private static void day1Vicky2() {
+		System.out.println("Day 1 vicky 2 codes Pushing into Vicky branch");
+	}
 	public static void main(String[] args) {
 	
 		method();
 		method1();
+		
+		day1Vicky1();
+		day1Vicky2();
+		
 	}
 }
